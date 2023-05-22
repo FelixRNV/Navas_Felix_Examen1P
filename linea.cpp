35b53dd6d43ca8c846f0de9715273204c19ee9d5
Felix Navas <fnavas@est.ups.edu.ec>
2023-05-22 04:56:09 -0500

    Commit Respal

--- linea.cpp
+++ linea.cpp
@@ -1,6 +1,30 @@
 #include "linea.h"
 
+float Linea::getD() const
+{
+    return d;
+}
+
+float Linea::getM() const
+{
+    return m;
+}
+
+Punto *Linea::getP1() const
+{
+    return p1;
+}
+
+Punto *Linea::getP2() const
+{
+    return p2;
+}
+
 Linea::Linea()
 {
 
 }
+
+Linea::Linea(Punto *p1, Punto *p2) : p1(p1),
+    p2(p2)
+{}
